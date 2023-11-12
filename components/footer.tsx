f819a89816e65d06371c9a22d5dc61488d13d303
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2023-11-12 16:12:53 +0700

    fixed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 		<footer className="bg-white border-t">
 			<div className="mx-auto py-10">
 				<div className="text-center text-xs text-black">
-					&copy; 2023 Alfan's Store, Inc. All rights reserved.
+					&copy; 2023 Alfan&apos;s Store, Inc. All rights reserved.
 				</div>
 			</div>
 		</footer>
